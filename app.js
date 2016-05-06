d4e6702f37071d1338e6c52c57d3e9dbbc66d2e9
sepehrvakili <sdvakili@gmail.com>
2016-05-06 15:21:40 -0700

    Solve all React components prompts

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    };
+  }
+  onListItemMouseOver (event) {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+  render() {
+    var style = {
+      'fontWeight': this.state.hover ? 'bold' : 'none'
+    }
+
+    return (
+      <li style={style} onMouseOver={this.onListItemMouseOver.bind(this)}>{this.props.name}</li>
+    );
+  }
+  
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.fruits.map( element => 
+      <GroceryListItem name={element} />
+    )}    
+  </ul>
+)
+
+ReactDOM.render(<GroceryList fruits={['Apple', 'Banana']}/>, document.getElementById("app"));
